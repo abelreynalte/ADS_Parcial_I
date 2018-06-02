7ea74146884efd777e1e25fec3c7be87201c49d7
abelrey <abelreynalte@hotmail.com>
2018-06-02 11:46:10 -0500

    primer commit

---
+++ src/Combinations.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FISI-Labo07
+ */
+public class Combinations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	Combinations combinations = new Combinations();
+		int n = 5;
+		System.out.println("# Ladder's steps: " + n);
+		System.out.println("# Step combinations: " + combinations.countCombinations(n, 0));
+    }
+    
+    public long countCombinations(int n, int level) {
+		if (n < 0) {
+			return 0;
+		}
+		if (n == 0 && level == 0) {
+			return 0;
+		}
+		if (n == 0 && level > 0) {
+			return 1;
+		}
+		return countCombinations(n - 1, level + 1) + countCombinations(n - 2, level + 1)
+				+ countCombinations(n - 3, level + 1);
+    }
+    
+}
